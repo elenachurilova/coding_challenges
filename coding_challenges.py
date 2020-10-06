70d8470279aea8858f379b3570b427152954c844
Elena Churilova <ellena.churilova@gmail.com>
2020-10-06 14:52:38 -0700

    Solved palindrome challenge

--- coding_challenges.py
+++ coding_challenges.py
@@ -1,2 +1,64 @@
 """A collection of solved coding challenges"""
 
+def is_palindrome(string):
+    """Return True if a given string is a palindrome
+
+    >>> is_palindrome("racecar")
+    True
+
+    >>> is_palindrome("snellens")
+    True
+
+    >>> is_palindrome("mother")
+    False
+    """
+
+    result = None
+    length = len(string)
+    middle = int(length/2)
+
+    if length % 2 != 0:
+        
+        # count1 = 0
+        # count2 = 0
+        # for i in range((length-middle), 0, -1):
+        #     # 2 1 0 
+        #     if string[(i-count1)] == string[(i+2+count2)]:
+        #         count1 -= 1
+        #         count2 += 1
+        #         continue
+
+        for i in range(1, middle+1):
+
+            if string[(middle-i)] == string[(middle+i)]:
+                result = True
+                continue
+            else:
+                result = False
+                break
+    
+    else:
+
+        prev = middle - 1
+
+        for i in range(0, middle):
+
+            if string[(middle+i)] == string[(prev-i)]:
+                result = True
+                continue
+            else:
+                result = False
+                break
+
+    return result
+
+
+
+
+
+
+
+if __name__ == '__main__':
+    import doctest
+    if doctest.testmod().failed == 0:
+        print('\n✨ ALL TESTS PASSED!\n')
