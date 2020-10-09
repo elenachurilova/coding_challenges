68733b87c1cc6a59d52eea5149bf348be780dd02
Elena Churilova <ellena.churilova@gmail.com>
2020-10-08 17:40:49 -0700

    Completed Repeated String

--- coding_challenges.py
+++ coding_challenges.py
@@ -19,15 +19,6 @@ def is_palindrome(string):
 
     if length % 2 != 0:
         
-        # count1 = 0
-        # count2 = 0
-        # for i in range((length-middle), 0, -1):
-        #     # 2 1 0 
-        #     if string[(i-count1)] == string[(i+2+count2)]:
-        #         count1 -= 1
-        #         count2 += 1
-        #         continue
-
         for i in range(1, middle+1):
 
             if string[(middle-i)] == string[(middle+i)]:
@@ -52,6 +43,31 @@ def is_palindrome(string):
 
     return result
 
+
+def repeatedString(s, n):
+    """Given sample string S and number N return count of 'a' chars 
+    in N first characters in an infinitely repeated S string
+    
+    >>> repeatedString("aba", 10)
+    7
+
+    >>> repeatedString("ojowrdcpavatfacuunxycyrmpbkvaxyrsgquwehhurnicgicmrpmgegftjszgvsgqavcrvdtsxlkxjpqtlnkjuyraknwxmnthfpt", 685118368975)
+    41107102139
+
+    >>> repeatedString("a", 1000000000000)
+    1000000000000
+    
+    """
+
+    length = len(s) 
+    coeff = int(round(n / length, 0)) 
+    bigger_string = coeff * length 
+    difference = bigger_string - n 
+    tail = s[0:(-difference)]
+    
+    count_in_final = tail.count('a') + ( s.count('a') * int(n / length) )    
+
+    return count_in_final
 
 
 
